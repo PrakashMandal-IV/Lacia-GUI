78fecf41cf7208078f7888c1e19c69c9e577302d
Prakash Mandal <pm1447616@gmail.com>
2022-02-12 19:23:12 +0530

    Update : some initial commands

--- Lacia.cs
+++ Lacia.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Speech.Synthesis;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lacia_GUI
 {
     
-    internal class Lacia
+    class Lacia
     {
+        
          Greet greet = new Greet();
         public void LaciaVoice(string input)
         {
@@ -23,5 +25,61 @@ namespace Lacia_GUI
         {
             LaciaVoice("Hello Sir, " + greet.DoGreet()+ ", This is Lacia, your Assistant, How can i help you ");
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public bool LaciaBody(string input)
+        {
+            
+            bool matched = false;
+            Thread.Sleep(1000);
+            if (input.Contains("time"))
+            {
+                LaciaVoice("It's," + DateTime.Now.ToString("h:mm tt"));
+                matched = true;
+            }
+            else if (input.Contains("date"))
+            {
+                LaciaVoice("It's," + DateTime.Now.ToString("dd/MM/yyyy"));
+                matched = true;
+            }
+            else if (input.Contains("who are you") || input.Contains("tell me about yourself"))
+            {
+                LaciaVoice("My Name is Lacia. Virtual Assistant. Just a sample for now");
+                matched = true;
+            }
+            else if ((input.Contains("you") && input.Contains("go off") || input.Contains("lacia") && input.Contains("go off")) && greet.DoGreet() == "Good Evening")
+            {
+                LaciaVoice("Going off, Good Night Sir");
+                matched = true;
+                GC.Collect();
+            }
+            else if (input.Contains("you") && input.Contains("go off") || input.Contains("lacia") && input.Contains("go off"))
+            {
+                LaciaVoice("Going off, Have a nice day sir");
+                matched = true;
+                GC.Collect();
+            }
+            else
+            {
+
+            }
+            return matched;
+
+        }
+        
+        
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,13 +13,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Speech.Synthesis;
+using System.Threading;
 
 namespace Lacia_GUI
 {
     public partial class MainWindow : Window
     {
-        bool started =false;
+        bool started = false;
         Lacia lacia = new Lacia();
+        
         
         public MainWindow()
         {
@@ -29,12 +31,30 @@ namespace Lacia_GUI
         }
         private async void Start_Click(object sender, RoutedEventArgs e)
         {
+            
             if(!started)
             {
+                started = true;
                 await Task.Run(() => lacia.Laciainit());
+                
             }
-
         }
-        
+        private async void Input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (started)
+            {
+                string input = Input.Text;
+                await Task.Run(() =>
+                {                  
+                    if(lacia.LaciaBody(input))
+                    {
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            Input.Text = null;
+                        });
+                    }
+                    });               
+            }
+        }       
     }
 }
