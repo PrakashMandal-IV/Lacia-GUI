3937b04763349e332ad96228ade20a973efcf2e9
Prakash Mandal <pm1447616@gmail.com>
2022-03-02 16:34:05 +0530

    Added: BackGround Image + UI changes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,34 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Speech.Synthesis;
-using System.Threading;
 using System.Speech.Recognition;
 using System.IO;
+using System.Drawing;
 
 namespace Lacia_GUI
 {
     public partial class MainWindow : Window
     {
         bool started = false; //check if hello initiated
-        Lacia lacia = new Lacia();
+        Lacia lacia = new Lacia();      
         SpeechRecognitionEngine listner = new SpeechRecognitionEngine();
         public MainWindow()
         {
             InitializeComponent();
-            StartListen();
+            StartListen();                     
+           
         }
+        
         private async void Start_Click(object sender, RoutedEventArgs e)
         {
             
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -592,6 +592,16 @@ namespace Lacia_GUI.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Lacia_Background {
+            get {
+                object obj = ResourceManager.GetObject("Lacia_Background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream LikeGames {
